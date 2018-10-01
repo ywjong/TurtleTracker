95e32fce5e834961b8e4893ab31c1f4a9bb7fad7
ywjong <yj107@duke.edu>
2018-10-01 12:29:08 -0400

    3.4a

--- ARGOSQueryTool.py
+++ ARGOSQueryTool.py
@@ -1,42 +1,5 @@
 # ARGOSQueryTool.py
 #
-# Description: Parses a line of ARGOS tracking data
-#
-# Created by: John Fay (john.fay@duke.edu)
-# Created on: Sept, 2018
-
-# Copy and paste a line of data as the startLine variable value
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
-
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split("\t")
-
-# Assign variables to specfic items in the list
-recordID = lineData[0]              # ARGOS tracking record ID
-obsDateTime = lineData[2]           # Observation date and time (combined)
-obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-obsLC = lineData[3]                 # Observation Location Class
-obsLat = lineData[5]                # Observation Latitude
-obsLon = lineData[6]                # Observation Longitude
-
-# Print information to the use
-print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLon,obsDate))
-
-fileObj = open('C:/Users/yj107/Documents/GitHub/TurtleTracker/Sara.txt','r')
-#C:\Users\yj107\Documents\GitHub\TurtleTracker\Sara.txt' does not work
-print(fileObj.readline())
-
-lineString = fileObj.readline(); print(lineString)
-lineList = fileObj.readlines(); print(lineList[-1])
-fileObj.close()
-
-newFile = open('newfile.txt','w')
-newFile.write("Hello world\nIt's me")
-newFile.close()
-
-# ARGOSQueryTool.py
-#
 # Description: Parses a line of ARGOS tracking data 
 #
 # Created by: John Fay (john.fay@duke.edu)
@@ -55,21 +18,20 @@ lineList = fileObj.readlines()
 # Closes the file object (now that we have all we need)
 fileObj.close()
 
-# Extract the first line from the lineList
-lineString = lineList[0]
+# Iterate through all the lines in the lineList
+for lineString in lineList:
 
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split("\t")
+    # Use the split command to parse the items in lineString into a list object
+    lineData = lineString.split("\t")
 
-# Assign variables to specfic items in the list
-recordID = lineData[0]              # ARGOS tracking record ID
-obsDateTime = lineData[2]           # Observation date and time (combined)
-obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-obsLC = lineData[3]                 # Observation Location Class
-obsLat = lineData[5]                # Observation Latitude
-obsLon = lineData[6]                # Observation Longitude
+    # Assign variables to specfic items in the list
+    recordID = lineData[0]              # ARGOS tracking record ID
+    obsDateTime = lineData[2]           # Observation date and time (combined)
+    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+    obsLC = lineData[3]                 # Observation Location Class
+    obsLat = lineData[5]                # Observation Latitude
+    obsLon = lineData[6]                # Observation Longitude
 
-# Print information to the user
-print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
-
+    # Print information to the user
+    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLat,obsDate))
